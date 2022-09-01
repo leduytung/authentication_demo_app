d83908cea932d781f53bc3f6ef071fd06d3edf9c
Kyle Le <kyle.le@deliveree.com>
2022-09-01 23:42:05 +0700

    Write simple specs

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, :type => :controller do
+  describe "GET index" do
+    it "redirect with unauthenticated user" do
+    end
+
+    it "success with authenticated user" do
+    end
+  end
+end
