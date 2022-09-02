614b959acd61eae79aabfef6bb0b69f97ad73d28
Kyle Le <kyle.le@deliveree.com>
2022-09-02 15:00:05 +0700

    Update the spec config

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe HomeController, :type => :controller do
   describe "GET index" do
     it "redirect with unauthenticated user" do
+      sign_in nil
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
     end
 
     it "success with authenticated user" do
+      sign_in
+      get :index
+      expect(response).to have_http_status(:success)
     end
   end
 end
